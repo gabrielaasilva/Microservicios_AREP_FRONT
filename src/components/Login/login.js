636e4d464c613d582141c69ceffb898ccba3135c
gabrielaasilva <anagabriela2700@gmail.com>
2022-04-20 22:37:09 -0500

    Fixing login

--- src/components/Login/login.js
+++ src/components/Login/login.js
@@ -1,29 +1,39 @@
 import { CognitoUser, AuthenticationDetails } from "amazon-cognito-identity-js";
 import UserPool from "../../utils/aws/UserPool";
 
-const user = new CognitoUser({
+function getValueInput(){
+  let inputName = document.getElementById("username").value;
+  let inputPassword = document.getElementById("psw").value;
+
+  console.log("username", inputName.value);
+  console.log("Psw", inputPassword.value);
+}
+
+const user = new CognitoUser ({
     Username: email,
     Pool: UserPool,
   });
 
-  const authDetails = new AuthenticationDetails({
-    Username: email,
-    Password: password,
-  });
+  
+  const authenticate = () => {
+    const authDetails = new AuthenticationDetails({
+      Username: email,
+      Password: password,
+    });
+    user.authenticateUser(authDetails, {
+      onSuccess: (data) => {
+        console.log("onSuccess: ", data);
+        window.location.href = "components/Feed/feed.html"
+      },
+      onFailure: (err) => {
+        console.error("onFailure: ", err);
+      },
+      newPasswordRequired: (data) => {
+        console.log("newPasswordRequired: ", data);
+      },
+    });
+  }
 
-
-  user.authenticateUser(authDetails, {
-    onSuccess: (data) => {
-      console.log("onSuccess: ", data);
-      window.location.href = "components/Feed/feed.html"
-    },
-    onFailure: (err) => {
-      console.error("onFailure: ", err);
-    },
-    newPasswordRequired: (data) => {
-      console.log("newPasswordRequired: ", data);
-    },
-  });
 
 
 // variables input, boton
